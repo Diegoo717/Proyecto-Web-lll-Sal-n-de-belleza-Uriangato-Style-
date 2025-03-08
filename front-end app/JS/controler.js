117c227818fa6f05e4fb88daacd4940f17c956a8
Diegoo717 <soydiegoo71@gmail.com>
2025-03-08 01:18:39 -0600

    Redirection

---
+++ front-end app/JS/controler.js
@@ -0,0 +1,29 @@
+const article1 = document.querySelector("#art-div-section-1")
+const article2 = document.querySelector("#art-div-section-2")
+const article3 = document.querySelector("#art-div-section-3")
+const article4 = document.querySelector("#art-div-section-4")
+const article5 = document.querySelector("#art-div-section-5")
+const article6 = document.querySelector("#art-div-section-6")
+const article7 = document.querySelector("#art-div-section-7")
+
+article1.addEventListener("click", (r) =>{
+    location.replace('http://127.0.0.1:5500/front-end%20app/HTML/serviciosMujer.html')
+})
+article2.addEventListener("click", (r) =>{
+    location.replace('http://127.0.0.1:5500/front-end%20app/HTML/serviciosHombre.html')
+})
+article3.addEventListener("click", (r) =>{
+    location.replace('')
+})
+article4.addEventListener("click", (r) =>{
+    location.replace('http://127.0.0.1:5500/front-end%20app/HTML/contacto.html')
+})
+article5.addEventListener("click", (r) =>{
+    location.replace('')
+})
+article6.addEventListener("click", (r) =>{
+    location.replace('')
+})
+article7.addEventListener("click", (r) =>{
+    location.replace('http://127.0.0.1:5500/front-end%20app/HTML/agendarCita.html')
+})

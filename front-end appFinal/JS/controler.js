a9b0c048d4a411109d621ee93b928c19e599f19c
Noemi Guzman Aguilera <98825719+NoemiGA01@users.noreply.github.com>
2025-03-12 23:10:31 -0600

    Add files via upload

---
+++ front-end appFinal/JS/controler.js
@@ -0,0 +1,29 @@
+const article1 = document.querySelector("#art-div-section-1");
+const article2 = document.querySelector("#art-div-section-2");
+const article3 = document.querySelector("#art-div-section-3");
+const article4 = document.querySelector("#art-div-section-4");
+const article5 = document.querySelector("#art-div-section-5");
+const article6 = document.querySelector("#art-div-section-6");
+const article7 = document.querySelector("#art-div-section-7");
+
+article1.addEventListener("click", () => {
+    window.location.href = "serviciosMujer.html";
+});
+article2.addEventListener("click", () => {
+    window.location.href = "serviciosHombre.html";
+});
+article3.addEventListener("click", () => {
+    window.location.href = "Niños.html";
+});
+article4.addEventListener("click", () => {
+    window.location.href = "contacto.html";
+});
+article5.addEventListener("click", () => {
+    window.location.href = "galeria.html";
+});
+article6.addEventListener("click", () => {
+    window.location.href = "Blog.html";
+});
+article7.addEventListener("click", () => {
+    window.location.href = "agendarCita.html";
+});

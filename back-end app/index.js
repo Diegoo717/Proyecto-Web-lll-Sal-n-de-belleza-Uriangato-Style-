931eb23a46ec0de305adf6fc8980a578e72bdda0
Diegoo717 <soydiegoo71@gmail.com>
2025-03-18 20:36:35 -0600

    Creación de la carpeta para la app back-end, Servidor configurado con express y creación de una api basica para pruebas

---
+++ back-end app/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path'); 
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../frontend-app')));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
